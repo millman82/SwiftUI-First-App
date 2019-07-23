e820fdbbdb2622608fa6eb346e431ba9193b8ad8
Tim Miller <millman@icloud.com>
2019-07-23 17:31:04 -0400

    Add animations and videeo indicator

--- Rooms/Rooms/RoomDetail.swift
+++ Rooms/Rooms/RoomDetail.swift
@@ -10,20 +10,41 @@ import SwiftUI
 
 struct RoomDetail: View {
     let room: Room
+    @State private var zoomed = false
     
     var body: some View {
-        Image(room.imageName)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .navigationBarTitle(Text(room.name), displayMode: .inline)
+        ZStack(alignment: .topLeading) {
+            Image(room.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .navigationBarTitle(Text(room.name), displayMode: .inline)
+                .tapAction {
+                    withAnimation(.easeInOut(duration: 2)) {
+                        self.zoomed.toggle()
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            
+            if room.hasVideo && !zoomed {
+                Image(systemName: "video.fill")
+                    .font(.title)
+                    .padding(.all)
+                    .transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
 #if DEBUG
 struct RoomDetail_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            RoomDetail(room: testData[0])
+        Group {
+            NavigationView {
+                RoomDetail(room: testData[0])
+            }
+            NavigationView {
+                RoomDetail(room: testData[1])
+            }
         }
     }
 }
